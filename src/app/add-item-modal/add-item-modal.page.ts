59153607367f228fc319c10ac6320de245c20827
Jaime González <jimmygzzmtz@gmail.com>
2019-09-23 00:34:45 -0500

    Items Storage
    Items, their names, categories, and colors are shown on the home page after being saved, and are also saved to storage.

--- src/app/add-item-modal/add-item-modal.page.ts
+++ src/app/add-item-modal/add-item-modal.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-item-modal',
@@ -8,28 +8,62 @@ import { ModalController } from '@ionic/angular';
 })
 export class AddItemModalPage implements OnInit {
 
+  itemNameString: any;
   itemName: any;
   selectedButtonId: string;
+  buttonAux: any;
 
-  constructor(public modalController: ModalController) {
+  constructor(public modalController: ModalController, public alertController: AlertController) {
   }
 
   ngOnInit() {
+  }
+
+  selectedButton($event){
+    this.itemName = document.querySelector('#itemName');
+    this.itemName.disabled = false
+    setTimeout(() => { this.itemName.setFocus(); }, 150);
+    if(this.selectedButtonId != $event.target.id){
+      this.buttonAux = document.querySelector('#' + $event.target.id)
+      this.buttonAux.fill = "outline"
+      if(this.selectedButtonId != undefined){
+        this.buttonAux = document.querySelector('#' + this.selectedButtonId)
+        this.buttonAux.fill = "solid"
+      }
+      this.selectedButtonId = $event.target.id
+    }
+    else{
+      this.buttonAux = document.querySelector('#' + this.selectedButtonId)
+      this.buttonAux.fill = "solid"
+      this.selectedButtonId = undefined
+      this.itemName.disabled = true
+    }
+    
   }
 
   dismiss(){
     this.modalController.dismiss();
   }
 
-  selectedButton($event){
-    this.itemName = document.querySelector('#itemName');
-    this.itemName.disabled = false
-    this.itemName.setFocus()
-    document.querySelector('#' + $event.target.id).fill = "outline"
-    if(this.selectedButtonId != undefined){
-      document.querySelector('#' + this.selectedButtonId).fill = "solid"
+  async save(){
+    if(this.selectedButtonId == undefined || this.itemNameString == undefined){
+      const alert = await this.alertController.create({
+        header: "Please select a category and enter a title.",
+        buttons: [
+          {
+              text: 'OK'
+          }
+      ]
+      });
+      await alert.present();
     }
-    this.selectedButtonId = $event.target.id
+    else{
+      var item = {
+        category: this.selectedButtonId,
+        name: this.itemNameString
+      } 
+      this.modalController.dismiss(item);
+    }
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import { ModalController } from '@ionic/angular';
 import { AddItemModalPage } from '../add-item-modal/add-item-modal.page';
 
@@ -7,10 +8,20 @@ import { AddItemModalPage } from '../add-item-modal/add-item-modal.page';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor(public modalController: ModalController) {
+  items: any = [];
 
+  constructor(private storage: Storage, public modalController: ModalController) {
+    this.storage.get('itemsArr').then((val) => {
+      if (val != "[]"){
+       this.items = JSON.parse(val)
+      }
+      else{
+       this.storage.set('itemsArr', JSON.stringify(this.items));
+      }
+    });
   }
 
   async addItem() {
@@ -21,8 +32,67 @@ export class HomePage {
     });
 
     modal.onDidDismiss()
-      .then(() => {
-        
+      .then((data) => {
+        if(data.data != undefined){
+          var category
+          var icon
+          var color
+          if(data.data.category == "barButton"){
+            category = "Bar"
+            icon = "beer"
+            color = "#0cd1e8"
+          }
+          if(data.data.category == "bookButton"){
+            category = "Book"
+            icon = "book"
+            color = "#ffce00"
+          }
+          if(data.data.category == "foodButton"){
+            category = "Food"
+            icon = "pizza"
+            color = "pink"
+          }
+          if(data.data.category == "ideaButton"){
+            category = "Idea"
+            icon = "bulb"
+            color = "orange"
+          }
+          if(data.data.category == "movieButton"){
+            category = "Movie"
+            icon = "videocam"
+            color = "#10dc60"
+          }
+          if(data.data.category == "musicButton"){
+            category = "Music"
+            icon = "musical-notes"
+            color = "#ffbe7d"
+          }
+          if(data.data.category == "personButton"){
+            category = "Person"
+            icon = "person"
+            color = "#7044ff"
+          }
+          if(data.data.category == "placeButton"){
+            category = "Place"
+            icon = "pin"
+            color = "#f04141"
+          }
+          if(data.data.category == "productButton"){
+            category = "Product"
+            icon = "cube"
+            color = "#ff8080"
+          }
+          if (this.items == null){
+            this.items = [];
+          }
+          this.items.push({
+            category: category,
+            name: data.data.name,
+            icon: icon,
+            color: color
+          })
+          this.storage.set('itemsArr', JSON.stringify(this.items));
+        }
     });
 
     await modal.present(); 
